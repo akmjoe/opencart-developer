bd8809b462ccc4995206fb169e79ad9e772cb4d8
Joe Rothrock <rothrockindustries@gmail.com>
2020-04-09 20:24:34 -0400

    Fixed showing .git folder in version list.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <ul>
 <?php
 foreach(scandir('.') as $dir) {
-	if(is_dir($dir) && $dir != '.' && $dir != '..') {
+	if(is_dir($dir) && $dir != '.' && $dir != '..' && $dir != '.git') {
 		echo '<li><a href="'.$dir.'/">'.$dir.'</a> <a href="'.$dir.'/admin/">Admin</a></li>';
 	}
 }
