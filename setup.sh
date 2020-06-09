e22c64c8330b896617db1cfe1d524dde88dbf9da
Joe Rothrock <rothrockindustries@gmail.com>
2020-06-08 20:42:18 -0400

    Added version 3.0.3.3

--- setup.sh
+++ setup.sh
@@ -35,20 +35,50 @@ setup() {
 	fi
 	# Set group owner
 	chgrp -f -R www-data system/storage/
+	chgrp -f -R www-data system/library/
 	chgrp -f -R www-data image/
 	chgrp -f -R www-data config.php
 	chgrp -f -R www-data admin/config.php
 	chgrp -f www-data admin/view/stylesheet/bootstrap.css
+	chgrp -f -R www-data admin/controller/extension/
+	chgrp -f -R www-data admin/language/
+	chgrp -f -R www-data admin/model/extension/
+	chgrp -f -R www-data admin/view/image/
+	chgrp -f -R www-data admin/view/javascript/
+	chgrp -f -R www-data admin/view/stylesheet/
+	chgrp -f -R www-data admin/view/template/extension/
+	chgrp -f -R www-data catalog/controller/extension/
+	chgrp -f -R www-data catalog/language/
+	chgrp -f -R www-data catalog/model/extension/
+	chgrp -f -R www-data catalog/view/javascript/
+	chgrp -f -R www-data catalog/view/theme/
+	chgrp -f -R www-data system/config/
+	chgrp -f -R www-data system/library/
+	chgrp -f -R www-data image/catalog/
 	# Set access permissions
 	chmod -f -R 0755 ./
+	chmod -f -R 0775 admin/controller/extension/
+	chmod -f -R 0775 admin/language/
+	chmod -f -R 0775 admin/model/extension/
+	chmod -f -R 0775 admin/view/image/
+	chmod -f -R 0775 admin/view/javascript/
+	chmod -f -R 0775 admin/view/stylesheet/
+	chmod -f -R 0775 admin/view/template/extension/
+	chmod -f -R 0775 catalog/controller/extension/
+	chmod -f -R 0775 catalog/language/
+	chmod -f -R 0775 catalog/model/extension/
+	chmod -f -R 0775 catalog/view/javascript/
+	chmod -f -R 0775 catalog/view/theme/
+	chmod -f -R 0775 system/config/
+	chmod -f -R 0775 system/library/
+	chmod -f -R 0775 image/catalog/
 	chmod -f 0775 system/storage/cache/
 	chmod -f 0775 system/storage/logs/
 	chmod -f 0775 system/storage/download/
 	chmod -f 0775 system/storage/upload/
 	chmod -f 0775 system/storage/modification/
-	chmod -f 0775 image/
-	chmod -f 0775 image/cache/
-	chmod -f 0775 image/catalog/
+	chmod -f 0775 system/storage/marketplace/
+	chmod -f -R 0775 image/
 	chmod -f 0775 config.php
 	chmod -f 0775 admin/config.php
 	chmod -f 0775 admin/view/stylesheet/bootstrap.css
